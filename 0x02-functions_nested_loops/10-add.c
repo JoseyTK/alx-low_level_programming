297089de2e962368753738f18ba929f80e9207de
joseph theuri <josektheuri@gmail.com>
2023-03-21 15:19:01 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two values
+ * @n1: the first number to be computed
+ * @n2: the second number to be computed
+ * retun : integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+}
