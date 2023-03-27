b3c4fff7af023d34a5b8898fa7163d1dfe8aacde
joseph theuri <josektheuri@gmail.com>
2023-03-27 13:09:26 -0700

    ./4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,18 +9,18 @@
 
 void print_rev(char *s)
 
-{ 
+{
 
 	int c = 0;
 
-	while (S[C] != '\0')
+	while (s[c] != '\0')
 	{
 		c++;
 	}
 
-	for (c -= 1; c>= 0; c--)
+	for (c -= 1; c >= 0; c--)
 	{
-		_putchar(S[C]);
+		_putchar(s[c]);
 	}
 	_putchar('\n');
 }
